22da6c0f3b3492950218613c5a819c76e375f06a
natto.and.eggs@gmail.com <natto.and.eggs@gmail.com>
2020-12-27 10:35:06 +0900

    refactor: directory structure

--- src/composables/use-todos-store-key.ts
+++
@@ -1,5 +0,0 @@
-import { InjectionKey } from "vue";
-import { TodosStore } from "@/composables/use-todos-store";
-
-const TodosStoreKey: InjectionKey<TodosStore> = Symbol("TodosStore");
-export default TodosStoreKey;
